22268a6ba8a0a899d8e286278daf3028a4590ab0
n2oneProgrammer <kubawojtasik70@gmail.com>
2022-07-16 00:07:20 +0200

    animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,16 +27,4 @@ public class GameManager : MonoBehaviour
     {
         isInMove = false;
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
