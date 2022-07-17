549bc39c6b1cd4e6b15dcae4a6f7c017b64724bd
Jaanonim <mat8mro@gmail.com>
2022-07-17 18:38:39 +0200

    Music setting

---
+++ Assets/SoundsSettings.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundsSettings : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    public string name;
+    public GameObject obj;
+
+    private void Start()
+    {
+        float v = PlayerPrefs.GetFloat(name, 0);
+        SetValue(v);
+    }
+
+    public void SetValue(float v)
+    {
+        obj.SetActive(v<0);
+        mixer.SetFloat(name, v);
+    }
+
+    public void Switch()
+    {
+        float v = PlayerPrefs.GetFloat(name, 0);
+        v = v == 0 ? -80 : 0;
+        PlayerPrefs.SetFloat(name, v);
+        SetValue(v);
+    }
+}
